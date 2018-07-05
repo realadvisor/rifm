450952a9374132610032bc4bef2c60194f95cd89
Ivan Starkov <istarkov@gmail.com>
2018-07-05 12:34:16 +0300

    Material example

--- docs/format.js
+++ docs/format.js
@@ -1,15 +1,17 @@
-export const numberFormat = str => {
+/* @flow */
+
+export const numberFormat = (str: string) => {
   const r = parseInt(str.replace(/[^\d]+/gi, ''), 10);
   return r ? r.toLocaleString('en') : '';
 };
 
-export const currencyFormat = str => {
+export const currencyFormat = (str: string) => {
   const clean = str.replace(/[^\d.]+/gi, '');
   const restoreDigits =
     clean.length > 2 && clean.indexOf('.') === -1
       ? `${clean.substr(0, clean.length - 2)}.${clean.substr(-2)}`
       : clean;
-  const r = parseFloat(restoreDigits, 10);
+  const r = parseFloat(restoreDigits);
   return r
     ? r.toLocaleString('de-CH', {
         minimumFractionDigits: 2,
