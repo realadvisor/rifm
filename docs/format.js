bd0f7c75a2bbd44801b82df62d3d2c35cf672536
Ivan Starkov <istarkov@gmail.com>
2018-07-05 12:44:51 +0300

    Improve format

--- docs/format.js
+++ docs/format.js
@@ -2,16 +2,22 @@
 
 export const numberFormat = (str: string) => {
   const r = parseInt(str.replace(/[^\d]+/gi, ''), 10);
+
   return r ? r.toLocaleString('en') : '';
 };
 
 export const currencyFormat = (str: string) => {
   const clean = str.replace(/[^\d.]+/gi, '');
-  const restoreDigits =
-    clean.length > 2 && clean.indexOf('.') === -1
-      ? `${clean.substr(0, clean.length - 2)}.${clean.substr(-2)}`
+
+  const beautify =
+    clean.length > 2
+      ? clean.indexOf('.') === -1
+        ? `${clean.substr(0, clean.length - 2)}.${clean.substr(-2)}`
+        : `${clean.split('.')[0]}.${clean.split('.')[1].substr(0, 2)}`
       : clean;
-  const r = parseFloat(restoreDigits);
+
+  const r = parseFloat(beautify);
+
   return r
     ? r.toLocaleString('de-CH', {
         minimumFractionDigits: 2,
