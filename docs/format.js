e210f28c4b0275234a8fb02f18535cd5acdb14fa
Ivan Starkov <istarkov@gmail.com>
2018-07-05 12:01:45 +0300

    Support for number with frational digit formatting

--- docs/format.js
+++ docs/format.js
@@ -1,4 +1,19 @@
 export const numberFormat = str => {
   const r = parseInt(str.replace(/[^\d]+/gi, ''), 10);
-  return r ? r.toLocaleString('ch') : '';
+  return r ? r.toLocaleString('en') : '';
+};
+
+export const currencyFormat = str => {
+  const clean = str.replace(/[^\d.]+/gi, '');
+  const restoreDigits =
+    clean.length > 2 && clean.indexOf('.') === -1
+      ? `${clean.substr(0, clean.length - 2)}.${clean.substr(-2)}`
+      : clean;
+  const r = parseFloat(restoreDigits, 10);
+  return r
+    ? r.toLocaleString('de-CH', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      })
+    : '';
 };
