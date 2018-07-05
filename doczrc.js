6860cf5bfcae1ddd18a33d75e2c83a85f014cc42
Ivan Starkov <istarkov@gmail.com>
2018-07-05 16:36:01 +0300

    Docz

---
+++ doczrc.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'Rifm - React Input Format & Mask',
+  description: 'Docemetation',
+  modifyBundlerConfig: config => {
+    config.output.publicPath = '/rifm';
+    return config;
+  },
+};
