7c874631da4df8d1cfe9e2b0ecf141fc679120c5
Ivan Starkov <istarkov@gmail.com>
2018-07-05 16:54:05 +0300

    Fixes

--- doczrc.js
+++ doczrc.js
@@ -1,8 +1,9 @@
 export default {
   title: 'Rifm - React Input Format & Mask',
   description: 'Docemetation',
+  base: '/rifm/',
   modifyBundlerConfig: config => {
-    config.output.publicPath = '/rifm';
+    config.output.publicPath = '/rifm/';
     return config;
   },
 };
