a21b1360d653ea2227d6c061a38a5b4c3404e5a8
Kevin Ross <kevin.ross@alienfast.com>
2019-01-14 14:02:49 -0500

    Add typescript types and testing (#40)
    * Added index.d.ts (TypeScript typings)
    
    * index.d.ts added to files list in package.json
    
    * Add typescript testing, fix type errors, add parameterized type for element in case another is used
    
    * type file moved to src

---
+++ tests/testTypescript.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Value } from 'react-powerplug';
+import { Rifm } from 'rifm';
+import { css } from 'emotion';
+
+const numberFormat = (str: string) => {
+  const r = parseInt(str.replace(/[^\d]+/gi, ''), 10);
+  return r ? r.toLocaleString('ch') : '';
+};
+
+const TestFlow = () => (
+  <Value initial={''}>
+    {text => (
+      <Rifm
+        replace={undefined}
+        value={text.value}
+        onChange={text.set}
+        format={numberFormat}
+      >
+        {({ value, onChange }) => (
+          <input
+            value={value}
+            onChange={onChange}
+            className={css({ textAlign: 'right' })}
+          />
+        )}
+      </Rifm>
+    )}
+  </Value>
+);
+
+() => <TestFlow />;
