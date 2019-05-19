570eddc5150c057d96fd23600e2d0a246227105d
Ivan Starkov <istarkov@gmail.com>
2019-05-19 21:59:02 +0300

    Switch RIFM on hooks

--- src/Rifm.js
+++ src/Rifm.js
@@ -16,162 +16,117 @@ type Props = {|
   }) => React.Node,
 |};
 
-type State = {|
-  value: string,
-  local: boolean,
-|};
+export const Rifm = (props: Props) => {
+  const [, refresh] = React.useReducer(c => c + 1, 0);
+  const valueRef = React.useRef(null);
+  const delRef = React.useRef(false);
 
-export class Rifm extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      value: props.value,
-      local: true,
-    };
-  }
-
-  _state: ?{|
-    before: string,
-    input: HTMLInputElement,
-    op: boolean,
-    di: boolean,
-    del: boolean,
-  |} = null;
-
-  _del: boolean = false;
-
-  _handleChange = (
+  const onChange = (
     evt: SyntheticInputEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    if (process.env.NODE_ENV !== 'production') {
-      if (evt.target.type === 'number') {
-        console.error(
-          'Rifm does not support input type=number, use type=tel instead.'
-        );
-        return;
-      }
-    }
-    // FUTURE: use evt.nativeEvent.inputType for del event, see comments at onkeydown
-    const stateValue = this.state.value;
-    let value = evt.target.value;
-    const input = evt.target;
-    const op = value.length > stateValue.length;
-    const del = this._del;
-    const noOp = stateValue === this.props.format(value);
+    const value = props.value;
+    const evtValue = evt.target.value;
 
-    this.setState({ value, local: true }, () => {
-      const { selectionStart } = input;
-      const refuse = this.props.refuse || /[^\d]+/g;
+    valueRef.current = [
+      evtValue, // target value
+      evt.target, // target itself
+      evtValue.length > value.length, // insert operation
+      delRef.current,
+      value === props.format(evtValue), // noOp
+    ];
+    // The main trick is to update underlying input with new value
+    // then get right input.selectionStart calculate there it should be
+    // and reupdate with masked/formatted value
+    refresh();
+  };
 
-      const before = value.substr(0, selectionStart).replace(refuse, '');
+  React.useLayoutEffect(() => {
+    if (valueRef.current != null) {
+      let [evtValue, input, op, del, noOp] = valueRef.current;
+      valueRef.current = null;
 
-      this._state = {
-        input,
-        before,
-        op,
-        di: del && noOp,
-        del,
-      };
+      // this usually occurs on deleting special symbols like ' here 123'123.00
+      // in case of del cursor should move differently vs backspace
+      const delWasNoOp = del && noOp;
 
-      if (this.props.replace && this.props.replace(stateValue) && op && !noOp) {
+      const refuse = props.refuse || /[^\d]+/g;
+
+      const before = evtValue
+        .substr(0, input.selectionStart)
+        .replace(refuse, '')
+        .toLowerCase();
+
+      const getStart = val => {
         let start = -1;
         for (let i = 0; i !== before.length; ++i) {
           start = Math.max(
             start,
-            value.toLowerCase().indexOf(before[i].toLowerCase(), start + 1)
+            val.toLowerCase().indexOf(before[i], start + 1)
           );
         }
+        return start;
+      };
 
-        const c = value.substr(start + 1).replace(refuse, '')[0];
-        start = value.indexOf(c, start + 1);
+      if (props.replace && props.replace(props.value) && op && !noOp) {
+        let start = getStart(evtValue);
 
-        value = `${value.substr(0, start)}${value.substr(start + 1)}`;
+        const c = evtValue.substr(start + 1).replace(refuse, '')[0];
+        start = evtValue.indexOf(c, start + 1);
+
+        evtValue = `${evtValue.substr(0, start)}${evtValue.substr(start + 1)}`;
       }
 
-      const fv = this.props.format(value);
+      const formattedValue = props.format(evtValue);
 
-      if (stateValue === fv) {
-        this.setState({ value });
+      if (props.value === formattedValue) {
+        refresh();
       } else {
-        this.props.onChange(fv);
-      }
-    });
-  };
-
-  // until https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/inputType will be supported
-  // by all major browsers (now supported by: +chrome, +safari, ?edge, !firefox)
-  // there is no way I found to distinguish in onChange
-  // backspace or delete was called in some situations
-  // firefox track https://bugzilla.mozilla.org/show_bug.cgi?id=1447239
-  _hKD = (evt: KeyboardEvent) => {
-    if (evt.code === 'Delete') {
-      this._del = true;
-    }
-  };
-
-  _hKU = (evt: KeyboardEvent) => {
-    if (evt.code === 'Delete') {
-      this._del = false;
-    }
-  };
-
-  static getDerivedStateFromProps(props: Props, state: State) {
-    return {
-      value: state.local ? state.value : props.value,
-      local: false,
-    };
-  }
-
-  render() {
-    const {
-      _handleChange,
-      state: { value },
-      props: { children },
-    } = this;
-
-    return children({ value, onChange: _handleChange });
-  }
-
-  // delete when  https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/inputType will be supported by all major browsers
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this._hKD);
-    document.removeEventListener('keyup', this._hKU);
-  }
-
-  // delete when  https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/inputType will be supported by all major browsers
-  componentDidMount() {
-    document.addEventListener('keydown', this._hKD);
-    document.addEventListener('keyup', this._hKU);
-  }
-
-  componentDidUpdate() {
-    const { _state } = this;
-
-    if (_state) {
-      const value = this.state.value;
-
-      let start = -1;
-      for (let i = 0; i !== _state.before.length; ++i) {
-        start = Math.max(
-          start,
-          value.toLowerCase().indexOf(_state.before[i].toLowerCase(), start + 1)
-        );
+        props.onChange(formattedValue);
       }
 
-      // format usually looks better without this
-      if (this.props.replace && (_state.op || (_state.del && !_state.di))) {
-        while (
-          value[start + 1] &&
-          (this.props.refuse || /[^\d]+/).test(value[start + 1])
-        ) {
-          start += 1;
+      return () => {
+        let start = getStart(formattedValue);
+
+        // format usually looks better without this
+        if (props.replace && (op || (del && !delWasNoOp))) {
+          while (
+            formattedValue[start + 1] &&
+            refuse.test(formattedValue[start + 1])
+          ) {
+            start += 1;
+          }
         }
-      }
 
-      _state.input.selectionStart = _state.input.selectionEnd =
-        start + 1 + (_state.di ? 1 : 0);
+        input.selectionStart = input.selectionEnd =
+          start + 1 + (delWasNoOp ? 1 : 0);
+      };
     }
+  });
 
-    this._state = null;
-  }
-}
+  React.useEffect(() => {
+    const handleKeyDown = (evt: KeyboardEvent) => {
+      if (evt.code === 'Delete') {
+        delRef.current = true;
+      }
+    };
+
+    const handleKeyUp = (evt: KeyboardEvent) => {
+      if (evt.code === 'Delete') {
+        delRef.current = false;
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  return props.children({
+    value: valueRef.current != null ? valueRef.current[0] : props.value,
+    onChange,
+  });
+};
