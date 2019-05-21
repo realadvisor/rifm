fd773d125062568a2dadff8d2e8a0065226d7e58
Ivan Starkov <istarkov@gmail.com>
2019-05-22 00:28:46 +0300

    change if wrap on more linear structure

--- src/Rifm.js
+++ src/Rifm.js
@@ -57,96 +57,92 @@ export const Rifm = (props: Props) => {
   // in both cases it's noop
   if (process.env.NODE_ENV === 'production' || typeof window !== 'undefined') {
     React.useLayoutEffect(() => {
-      if (valueRef.current != null) {
-        let [
-          eventValue,
-          input,
-          isSizeIncreaseOperation,
-          isDeleleteButtonDown,
-          // No operation means that value itself hasn't been changed, BTW cursor, selection etc can be changed
-          isNoOperation,
-        ] = valueRef.current;
-        valueRef.current = null;
+      if (valueRef.current == null) return;
 
-        // this usually occurs on deleting special symbols like ' here 123'123.00
-        // in case of isDeleleteButtonDown cursor should move differently vs backspace
-        const deleteWasNoOp = isDeleleteButtonDown && isNoOperation;
+      let [
+        eventValue,
+        input,
+        isSizeIncreaseOperation,
+        isDeleleteButtonDown,
+        // No operation means that value itself hasn't been changed, BTW cursor, selection etc can be changed
+        isNoOperation,
+      ] = valueRef.current;
+      valueRef.current = null;
 
-        // Create string from only accepted symbols
-        const clean = str => (str.match(props.accept || /\d/g) || []).join('');
+      // this usually occurs on deleting special symbols like ' here 123'123.00
+      // in case of isDeleleteButtonDown cursor should move differently vs backspace
+      const deleteWasNoOp = isDeleleteButtonDown && isNoOperation;
 
-        const valueBeforeSelectionStart = clean(
-          eventValue.substr(0, input.selectionStart)
-        ).toLowerCase();
+      // Create string from only accepted symbols
+      const clean = str => (str.match(props.accept || /\d/g) || []).join('');
 
-        // trying to find cursor position in formatted value having knowledge about valueBeforeSelectionStart
-        // This works because we assume that format doesn't change the order of accepted symbols.
-        // Imagine we have formatter which adds ' symbol between numbers, and by default we refuse all non numeric symbols
-        // for example we had input = 1'2|'4 (| means cursor position) then user entered '3' symbol
-        // inputValue = 1'23'|4 so valueBeforeSelectionStart = 123 and formatted value = 1'2'3'4
-        // calling getCursorPosition("1'2'3'4") will give us position after 3, 1'2'3|'4
-        // so for formatting just this function to determine cursor position after formatting is enough
-        // with masking we need to do some additional checks see `replace` below
-        const getCursorPosition = val => {
-          let start = 0;
-          for (let i = 0; i !== valueBeforeSelectionStart.length; ++i) {
-            start = Math.max(
-              start,
-              val.toLowerCase().indexOf(valueBeforeSelectionStart[i], start) + 1
-            );
-          }
-          return start;
-        };
+      const valueBeforeSelectionStart = clean(
+        eventValue.substr(0, input.selectionStart)
+      ).toLowerCase();
 
-        // Masking part, for masks if size of mask is above some value (props.replace checks that)
-        // we need to replace symbols instead of do nothing as like in format
+      // trying to find cursor position in formatted value having knowledge about valueBeforeSelectionStart
+      // This works because we assume that format doesn't change the order of accepted symbols.
+      // Imagine we have formatter which adds ' symbol between numbers, and by default we refuse all non numeric symbols
+      // for example we had input = 1'2|'4 (| means cursor position) then user entered '3' symbol
+      // inputValue = 1'23'|4 so valueBeforeSelectionStart = 123 and formatted value = 1'2'3'4
+      // calling getCursorPosition("1'2'3'4") will give us position after 3, 1'2'3|'4
+      // so for formatting just this function to determine cursor position after formatting is enough
+      // with masking we need to do some additional checks see `replace` below
+      const getCursorPosition = val => {
+        let start = 0;
+        for (let i = 0; i !== valueBeforeSelectionStart.length; ++i) {
+          start = Math.max(
+            start,
+            val.toLowerCase().indexOf(valueBeforeSelectionStart[i], start) + 1
+          );
+        }
+        return start;
+      };
+
+      // Masking part, for masks if size of mask is above some value (props.replace checks that)
+      // we need to replace symbols instead of do nothing as like in format
+      if (
+        props.replace &&
+        props.replace(props.value) &&
+        isSizeIncreaseOperation &&
+        !isNoOperation
+      ) {
+        let start = getCursorPosition(eventValue);
+
+        const c = clean(eventValue.substr(start))[0];
+        start = eventValue.indexOf(c, start);
+
+        eventValue = `${eventValue.substr(0, start)}${eventValue.substr(
+          start + 1
+        )}`;
+      }
+
+      const formattedValue = props.format(eventValue);
+
+      if (props.value === formattedValue) {
+        // if nothing changed for formatted value, just refresh so props.value will be used at render
+        refresh();
+      } else {
+        props.onChange(formattedValue);
+      }
+
+      return () => {
+        let start = getCursorPosition(formattedValue);
+
+        // Visually improves working with masked values,
+        // like cursor jumping over refused symbols
         if (
           props.replace &&
-          props.replace(props.value) &&
-          isSizeIncreaseOperation &&
-          !isNoOperation
+          (isSizeIncreaseOperation || (isDeleleteButtonDown && !deleteWasNoOp))
         ) {
-          let start = getCursorPosition(eventValue);
-
-          const c = clean(eventValue.substr(start))[0];
-          start = eventValue.indexOf(c, start);
-
-          eventValue = `${eventValue.substr(0, start)}${eventValue.substr(
-            start + 1
-          )}`;
-        }
-
-        const formattedValue = props.format(eventValue);
-
-        if (props.value === formattedValue) {
-          // if nothing changed for formatted value, just refresh so props.value will be used at render
-          refresh();
-        } else {
-          props.onChange(formattedValue);
-        }
-
-        return () => {
-          let start = getCursorPosition(formattedValue);
-
-          // Visually improves working with masked values,
-          // like cursor jumping over refused symbols
-          if (
-            props.replace &&
-            (isSizeIncreaseOperation ||
-              (isDeleleteButtonDown && !deleteWasNoOp))
-          ) {
-            while (
-              formattedValue[start] &&
-              clean(formattedValue[start]) === ''
-            ) {
-              start += 1;
-            }
+          while (formattedValue[start] && clean(formattedValue[start]) === '') {
+            start += 1;
           }
+        }
 
-          input.selectionStart = input.selectionEnd =
-            start + (deleteWasNoOp ? 1 : 0);
-        };
-      }
+        input.selectionStart = input.selectionEnd =
+          start + (deleteWasNoOp ? 1 : 0);
+      };
     });
   }
 
