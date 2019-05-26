a8220f49384ac0daaec660ed667dbcffa7854673
Ivan Starkov <istarkov@gmail.com>
2019-05-26 17:44:46 +0300

    Add non fixed numbers failing tests

--- tests/formatters.test.js
+++ tests/formatters.test.js
@@ -48,3 +48,34 @@ test("formatNumber check that format doesn't change original value", () => {
     }
   }
 });
+
+test('formatNumber fixed must return equal to scale amount of digits at fractional part', () => {
+  const prng = new Alea(100230);
+  for (let scale = 0; scale !== 5; ++scale) {
+    const formatFixed = v => formatNumber(v, scale, true);
+    for (let i = 0; i !== 1000; ++i) {
+      const val = prng() * i;
+      const [, tail] = formatFixed(`${val}`)
+        .toString()
+        .split('.');
+      expect(tail != null ? tail.length : 0).toEqual(scale);
+    }
+  }
+});
+
+test('formatNumber non fixed must not allow 0 as scale position', () => {
+  // because parseFloat will always remove that 0, and at least at scale position we can prevent it
+  const formatNonFixed1 = v => formatNumber(v, 1, false);
+  const formatNonFixed2 = v => formatNumber(v, 2, false);
+  const formatNonFixed3 = v => formatNumber(v, 3, false);
+  const formatNonFixed4 = v => formatNumber(v, 4, false);
+  expect(formatNonFixed1('0.0')).toEqual('0.');
+  expect(formatNonFixed2('0.10')).toEqual('0.1');
+  expect(formatNonFixed3('0.110')).toEqual('0.11');
+  expect(formatNonFixed4('0.1110')).toEqual('0.111');
+});
+
+test('formatNumber non fixed must allow 0 otherwise impossible to enter 0.101', () => {
+  const formatNonFixed = v => formatNumber(v, 3, false);
+  expect(formatNonFixed('0.1')).toEqual('0.10');
+});
