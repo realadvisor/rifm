1c9385c84d61458c963422b2734d4f6f3c225257
Ivan Starkov <istarkov@gmail.com>
2019-05-26 13:30:20 +0300

    Fix resolutions, fix index

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 import * as React from 'react';
 import { Container, Typography, Link } from '@material-ui/core';
 
--- pages/phone-format/index.js
+++ pages/phone-format/index.js
@@ -1,4 +1,5 @@
 /* @flow */
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Rifm } from 'rifm';
