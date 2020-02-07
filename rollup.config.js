8374f2e198c393c3fa861ccccb4239f2bbe9a802
Bogdan Chadkin <trysound@yandex.ru>
2020-02-07 23:13:07 +0300

    Upgrade rollup
    Cleaned up size snapshot and simplified rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,15 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import pkg from './package.json';
 
 const input = './src/index.js';
-
-const external = id => !id.startsWith('.') && !id.startsWith('/');
-
-const globals = { react: 'React' };
-
 const name = 'Rifm';
+const globals = { react: 'React' };
+const external = Object.keys(globals);
 
 const babelOptions = {
-  exclude: '**/node_modules/**',
   babelrc: false,
   presets: [['@babel/env', { loose: true }], '@babel/flow', '@babel/react'],
 };
@@ -23,24 +18,20 @@ export default [
   {
     input,
     output: { file: 'dist/rifm.umd.js', format: 'umd', name, globals },
-    external: Object.keys(globals),
+    external,
     plugins: [
-      nodeResolve(),
       babel(babelOptions),
       replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
-      sizeSnapshot(),
     ],
   },
 
   {
     input,
     output: { file: 'dist/rifm.min.js', format: 'umd', name, globals },
-    external: Object.keys(globals),
+    external,
     plugins: [
-      nodeResolve(),
       babel(babelOptions),
       replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      sizeSnapshot(),
       terser(),
     ],
   },
@@ -49,7 +40,7 @@ export default [
     input,
     output: { file: pkg.main, format: 'cjs' },
     external,
-    plugins: [babel(babelOptions), sizeSnapshot()],
+    plugins: [babel(babelOptions)],
   },
 
   {
